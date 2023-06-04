ea5a3ef73b2c9ef5541d0e80cf87b9cc95df1774
John Perez <mrjohnjairo10@gmail.com>
2023-06-04 18:37:02 -0500

    Fix linter errors
    - Fix linting errors in the back using `clippy`
    - Fix linter rule for `vite.config.ts`

--- backend/src/config/mongo.rs
+++ backend/src/config/mongo.rs
@@ -14,8 +14,8 @@ impl DBMongo {
     pub fn init() -> Self {
         dotenv().ok();
         let uri = match env::var("MONGOURI") {
-            Ok(v) => v.to_string(),
-            Err(_) => format!("Error loading env variable:"),
+            Ok(v) => v,
+            Err(_) => "Error loading env variable:".to_string(),
         };
         let client = Client::with_uri_str(uri).unwrap();
         let db = client.database("Or-it-Cluster");
@@ -33,26 +33,20 @@ impl DBMongo {
             email: new_owner.email.clone(),
             phone: new_owner.phone.clone(),
         };
-        let col = DBMongo::col_helper::<Owner>(&self, "owner");
-        let data = col
-            .insert_one(new_doc, None)
-            .ok()
-            .expect("Error creating owner");
+        let col = DBMongo::col_helper::<Owner>(self, "owner");
+        let data = col.insert_one(new_doc, None).expect("Error creating owner");
         let new_owner = Owner {
             _id: data.inserted_id.as_object_id(),
             name: new_owner.name.clone(),
             email: new_owner.email.clone(),
-            phone: new_owner.phone.clone(),
+            phone: new_owner.phone,
         };
         Ok(new_owner)
     }
 
     pub fn get_owners(&self) -> Result<Vec<Owner>, Error> {
-        let col = DBMongo::col_helper::<Owner>(&self, "owner");
-        let cursors = col
-            .find(None, None)
-            .ok()
-            .expect("Error getting list of owners");
+        let col = DBMongo::col_helper::<Owner>(self, "owner");
+        let cursors = col.find(None, None).expect("Error getting list of owners");
         let owners: Vec<Owner> = cursors.map(|doc| doc.unwrap()).collect();
         Ok(owners)
     }
@@ -60,10 +54,9 @@ impl DBMongo {
     pub fn single_owner(&self, id: &String) -> Result<Owner, Error> {
         let obj_id = ObjectId::parse_str(id).unwrap();
         let filter = doc! {"_id": obj_id};
-        let col = DBMongo::col_helper::<Owner>(&self, "owner");
+        let col = DBMongo::col_helper::<Owner>(self, "owner");
         let owner_detail = col
             .find_one(filter, None)
-            .ok()
             .expect("Error getting owner's detail");
         Ok(owner_detail.unwrap())
     }
@@ -75,29 +68,25 @@ impl DBMongo {
             name: new_project.name.clone(),
             description: new_project.description.clone(),
             owner_id: new_project.owner_id.clone(),
-            status: new_project.status.clone(),
+            status: new_project.status,
         };
-        let col = DBMongo::col_helper::<Project>(&self, "project");
+        let col = DBMongo::col_helper::<Project>(self, "project");
         let data = col
             .insert_one(new_doc, None)
-            .ok()
             .expect("Error creating project");
         let new_project = Project {
             _id: data.inserted_id.as_object_id(),
             name: new_project.name.clone(),
             description: new_project.description.clone(),
             owner_id: new_project.owner_id.clone(),
-            status: new_project.status.clone(),
+            status: new_project.status,
         };
         Ok(new_project)
     }
 
     pub fn get_projects(&self) -> Result<Vec<Project>, Error> {
-        let col = DBMongo::col_helper::<Project>(&self, "project");
-        let cursors = col
-            .find(None, None)
-            .ok()
-            .expect("Error getting lit of projects");
+        let col = DBMongo::col_helper::<Project>(self, "project");
+        let cursors = col.find(None, None).expect("Error getting lit of projects");
         let projects: Vec<Project> = cursors.map(|doc| doc.unwrap()).collect();
         Ok(projects)
     }
@@ -105,10 +94,9 @@ impl DBMongo {
     pub fn single_project(&self, id: &String) -> Result<Project, Error> {
         let obj_id = ObjectId::parse_str(id).unwrap();
         let filter = doc! {"_id": obj_id};
-        let col = DBMongo::col_helper::<Project>(&self, "project");
+        let col = DBMongo::col_helper::<Project>(self, "project");
         let project_detail = col
             .find_one(filter, None)
-            .ok()
             .expect("Error getting project's detail");
         Ok(project_detail.unwrap())
     }
